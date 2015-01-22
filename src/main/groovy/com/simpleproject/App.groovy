b23417c3b8bc2f8f18b1d739cc11a3f02cc8b3b8
Kevin Tan Hong Ann <kevintanhongann@gmail.com>
2015-01-22 13:22:20 +0800

    updated groovy version to 2.4 final

--- src/main/groovy/com/simpleproject/App.groovy
+++ src/main/groovy/com/simpleproject/App.groovy
@@ -1,17 +1,25 @@
 package com.simpleproject
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.context.annotation.ComponentScan
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 
 @RestController
 @EnableAutoConfiguration
+@ComponentScan
 class App {
 
     @RequestMapping("/")
     String home(){
         "Hello World!"
+    }
+
+
+    @RequestMapping("/testjson")
+    String jsonOutputTest(){
+
     }
 
 
