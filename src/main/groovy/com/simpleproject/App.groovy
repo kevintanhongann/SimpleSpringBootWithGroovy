55905b2360d05528718f96396403dfa619597abe
Kevin Tan Hong Ann <kevintanhongann@gmail.com>
2015-01-31 13:46:29 +0800

    successsfully render a page

--- src/main/groovy/com/simpleproject/App.groovy
+++ src/main/groovy/com/simpleproject/App.groovy
@@ -1,9 +1,12 @@
 package com.simpleproject
 
+import com.simpleproject.domain.Person
+import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.ModelAndView
 
 
 @RestController
@@ -12,15 +15,17 @@ import org.springframework.web.bind.annotation.RestController
 class App {
 
     @RequestMapping("/")
-    String home(){
-        "Hello World!"
+    def home(){
+        /*new ModelAndView(
+                "views/main",
+                [bootVersion: Banner.package.implementationVersion,
+                 groovyVersion: GroovySystem.version])*/
+        new ModelAndView("views/home")
     }
 
 
-    @RequestMapping("/testjson")
-    String jsonOutputTest(){
-
-    }
-
-
+    /*@RequestMapping("list")
+    def list(){
+        new ModelAndView('views/person/list', [persons: Person.list()])
+    }*/
 }
